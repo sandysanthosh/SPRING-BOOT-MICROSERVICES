8440bfe5538086a4ba3ed169cc3f6daadb02dcfe
Santhosh Kumar Karthikeyan <santhosh072018@gmail.com>
2026-09-19 23:09:02 +0530

    test: add echo endpoint integration test

---
+++ src/test/java/com/yourname/EchoControllerIntegrationTest.java
@@ -0,0 +1,39 @@
+package com.yourname;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@WebAppConfiguration
+public class EchoControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void echoesRequestMessageAsJson() throws Exception {
+        mockMvc.perform(get("/api/echo").param("message", "hello"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.message").value("hello"));
+    }
+}
