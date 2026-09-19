4aa4fb2a8566ecf7634c11b38e0973d2ea871a48
Santhosh Kumar Karthikeyan <santhosh072018@gmail.com>
2026-09-19 22:53:05 +0530

    feat: add ping API with integration test
    * feat: add ping endpoint
    
    * test: add ping endpoint integration test

---
+++ src/test/java/com/yourname/PingControllerIntegrationTest.java
@@ -0,0 +1,40 @@
+package com.yourname;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Main.class)
+@WebAppConfiguration
+public class PingControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setUp() {
+        mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void returnsPong() throws Exception {
+        mockMvc.perform(get("/api/ping"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("pong"));
+    }
+}
