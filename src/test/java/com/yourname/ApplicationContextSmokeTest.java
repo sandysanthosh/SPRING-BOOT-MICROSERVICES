816ebb0d14eb1c4b6506365527e3deee8ce1698f
Santhosh Kumar Karthikeyan <santhosh072018@gmail.com>
2026-09-19 22:49:42 +0530

    test: verify RestTemplate bean availability

--- src/test/java/com/yourname/ApplicationContextSmokeTest.java
+++ src/test/java/com/yourname/ApplicationContextSmokeTest.java
@@ -1,16 +1,28 @@
 package com.yourname;
 
+import static org.junit.Assert.assertNotNull;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Main.class)
 public class ApplicationContextSmokeTest {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @Test
     public void contextLoads() {
         // The test passes when the Spring application context starts successfully.
+    }
+
+    @Test
+    public void restTemplateBeanIsAvailable() {
+        assertNotNull(restTemplate);
     }
 }
